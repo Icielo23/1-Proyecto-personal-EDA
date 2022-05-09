466cea448c7f025d7a62d9089fb1e28004c51dcb
Icielo23 <viquise@unsa.edu.pe>
2022-05-08 23:13:04 -0500

    Mi nombre

--- Holamundo.java
+++ Holamundo.java
@@ -2,5 +2,6 @@ public class Holamundo {
     public static void main(String[] args) {
       System.out.println("Hola mundo");
       System.out.println("Bienvenidos al curso de EDA");
+      System.out.println("Valery Cielo Iquise Mamani");
     }
 }
